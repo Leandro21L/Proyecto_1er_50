5dfdd4d1e7a47791204d702772dcd6570da2d054
Leandro Luis <leandro.luis@uptc.edu.co>
2023-09-19 02:04:42 -0500

    clase Libary creada

--- src/co/edu/uptc/model/Headquarter.java
+++ src/co/edu/uptc/model/Headquarter.java
@@ -2,9 +2,23 @@ package co.edu.uptc.model;
 
 public class Headquarter {
 
+    private String name;
     private String[] campus;
 
     public Headquarter() {
+    }
+
+    public Headquarter(String name, String[] campus) {
+        this.name = name;
+        this.campus = campus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String[] getCampus() {
---
+++ src/co/edu/uptc/model/Library.java
@@ -0,0 +1,23 @@
+package co.edu.uptc.model;
+
+public class Library {
+
+    public Library() {
+    }
+
+    public void createBook(){
+
+    }
+
+    public void deleteBook(){
+
+    }
+
+    public void searchBook(){
+
+    }
+
+    public void balanceAVLTree(){
+        
+    }
+}
